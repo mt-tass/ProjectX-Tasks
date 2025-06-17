5af4cd66acbcd7daa4d6413f26f1684b3c69f606
mt-tass <mustafataherramp@gmail.com>
2025-06-17 23:11:31 +0530

    Create solution.py
    Solution for Task 1 : Lost in Translation

---
+++ Task1/solution.py
@@ -0,0 +1,68 @@
+import re
+from symspellpy import SymSpell
+import nltk
+from nltk.corpus import words
+import spacy
+import os
+
+eng_dict = set(w.lower() for w in words.words())
+nlp = spacy.load("en_core_web_sm")
+
+sym_spell = SymSpell(max_dictionary_edit_distance=1)
+sym_spell.load_dictionary("en-80k.txt", term_index=0, count_index=1)
+
+def correct_word(word):
+    raw = word
+    lower = word.lower()
+    
+    if lower in eng_dict:
+        return word 
+    suggestions = sym_spell.lookup(word, verbosity=2)
+    if suggestions:
+        best = suggestions[0].term
+        return restore_case(word, best)
+    else:
+        return word
+def restore_case(og, corrected):
+    if og.istitle():
+        return corrected.capitalize()
+    elif og.isupper():
+        return corrected.upper()
+    else:
+        return corrected
+
+def restore_sentence(text):
+    restored = []
+    for word in re.findall(r'\w+|\W+', text):
+        if word.strip() and any(c.isalpha() for c in word):
+            restored.append(correct_word(word))
+        else:
+            restored.append(word)
+    return ''.join(restored)
+
+def get_nouns(line):
+    line_text = nlp(line)
+    nouns = []
+    for entity in line_text.ents:
+        if entity.label_ in ["PERSON", "ORG", "GPE", "FAC", "LOC", "PRODUCT", "EVENT"] :
+            nouns.append(entity.text)
+    return sorted(nouns)
+
+test_cases = [
+    "In April 2023, Sundar Pichai did announce that Google would be launehing a new AI product namcd Gemini. Barack Obama also gave a speech at Harvard University, cmphasizing the role of technology in modern education.",
+    "Project X is an exclusive elub at Veermata Jijabai Technological Institute, Mumbai, mcant to 5erve as a healthy environment for 5tudents to learn from each other and grow together. Through the guidance of their mcntors these 5tudents are able to complete daunting tasks.",
+    "I will be eompleting my BTech dcgree in Mechanical Engineering from VJTI in 2028",
+    "However the rcsults were clear"
+]
+i = 0
+pattern = r'\b(However)\b(?![\w,.;:!?])'
+def get_comma(text):
+	return re.sub(pattern,r'\1,',text)
+for test_case in test_cases:
+    print(f"\nTestCase = {i + 1}")
+    restored = restore_sentence(test_case)
+    nouns = get_nouns(restored)
+    final = get_comma(restored)
+    print("corrected=", final)
+    print("nouns=", nouns)
+    i=i+1
